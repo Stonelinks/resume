212989c07731d346ca95bf7c61f0616c012ff4d2
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-08-04 19:18:34 +0900

    start moving things over to wintersmith

---
+++ Gruntfile.js
@@ -0,0 +1,120 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    config: {
+      src: 'contents',
+      templates: 'templates',
+      dist: 'build'
+    },
+
+    wintersmith_config: grunt.file.readJSON('config.json'),
+
+    wintersmith: {
+
+      build: {},
+
+      preview: {
+        options: {
+          action: 'preview'
+        }
+      }
+    },
+
+    watch: {
+
+      less: {
+        files: ['<%= config.src %>/less/**/*.less'],
+        tasks: ['less']
+      }
+    },
+
+    clean: ['<%= config.dist %>'],
+
+    less: {
+
+      main: {
+        files: {
+          '<%= config.src %>/css/main.css': '<%= config.src %>/less/main.less'
+        }
+      }
+    },
+
+    concurrent: {
+      server: ['wintersmith:preview', 'watch'],
+      options: {
+        logConcurrentOutput: true
+      }
+    },
+
+    copy: {
+      nojekyll: {
+        src: '.nojekyll',
+        dest: '<%= config.dist %>/'
+      }
+    },
+
+    'link-checker': {
+      dev: {
+        site: 'localhost',
+        options: {
+          noFragment: true,
+          initialPort: '<%= wintersmith_config.port + 2 %>'
+        }
+      }
+    },
+
+    connect: {
+      test: {
+        options: {
+          port: '<%= wintersmith_config.port + 2 %>',
+          base: 'build'
+        }
+      }
+    },
+
+    'gh-pages': {
+      options: {
+        base: '<%= config.dist %>',
+        branch: 'master'
+      },
+      src: ['**', '.*']
+    }
+  });
+
+  grunt.registerTask('pre', [
+    'clean',
+    'less'
+  ]);
+
+  grunt.registerTask('build', [
+    'pre',
+    'wintersmith:build',
+    'copy:nojekyll'
+  ]);
+
+  grunt.registerTask('publish', [
+    'build',
+    'gh-pages'
+  ]);
+
+  grunt.registerTask('server', [
+    'pre',
+    'concurrent:server'
+  ]);
+
+  grunt.registerTask('test', [
+    'build',
+    'connect:test',
+    'link-checker'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+};
