c08a4d3b9c127e58e8ddb65b4e23b4d9576aacbd
Lucas Doyle <lucas.p.doyle@gmail.com>
2025-04-11 13:16:35 -0700

    delete python / aider stuff

--- env.sh
+++ env.sh
@@ -46,44 +46,6 @@ fi
 
 PATH=$PATH:$GOPATH/bin
 
-# python
-PYTHON_VERSION=3.12.9
-export PYTHON_VERSION
-
-export UV_VERSION="0.6.2"
-export UV_UNMANAGED_INSTALL=$OPT_DIR/uv-$UV_VERSION
-export UV_PYTHON_INSTALL_DIR=$OPT_DIR/python
-export UV_LINK_MODE=copy
-export UV_NO_CACHE=true
-export UV_PYTHON_PREFERENCE=only-managed
-
-PATH=$UV_UNMANAGED_INSTALL:$PATH
-
-export VIRTUAL_ENV=$OPT_DIR/venv
-
-# if the venv exists...
-if [ -d $VIRTUAL_ENV ]; then
-	PATH=$VIRTUAL_ENV/bin:$PATH
-	source $VIRTUAL_ENV/bin/activate
-fi
-
-# local site-packages
-LOCAL_SITE_PACKAGES=$VIRTUAL_ENV/lib/python$PY_VERSION_MAJOR.$PY_VERSION_MINOR/site-packages
-
-PYTHONPATH=$LOCAL_SITE_PACKAGES:$PYTHONPATH
-
-# local python source
-# PYTHONPATH=$PROJECT_ROOT/something:$PYTHONPATH
-
-export PYTHONPATH
-
-export PYRIGHT_PYTHON_FORCE_VERSION="latest"
-
-# check if python is in the path, and its the one in the venv
-if [ "$(which python)" != "$VIRTUAL_ENV/bin/python" ]; then
-	echo "WARNING: Python not in path, or not the one in the venv"
-fi
-
 # Node
 # bun
 
